6dc9eb8dba8a5565c90b20c484afc272a26741ab
marsabatini <marco_rsabatini@hotmail.com>
2023-07-09 08:11:30 -0300

    Desafio 03 (v. 1.01) - Encontra e imprime substrings com palíndromos

---
+++ DesafioDeCod_PwC/src/main/Desafio_03/Main.java
@@ -0,0 +1,90 @@
+package main.Desafio_03;
+
+/**
+ * DESAFIO 03
+ * <p>
+ * O código abaixo encontra e imprime uma substring palíndroma,
+ * caso exista, a partir de uma string maior.
+ * <p>
+ * **** Exemplo:
+ * **** -> Input: babad
+ * **** -> Output: bab
+ *
+ * @autor: Marco Sabatini
+ * @version: 1.01
+ * @since: 08/07/2023
+ **/
+
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        String strExemplo = "babad";
+        String palindromo;
+        Scanner enter = new Scanner(System.in);
+
+        System.out.println("DESAFIO 03");
+        System.out.println("Encontrar palíndromos.\n");
+        System.out.println("---------------------\n");
+        System.out.print("Digite 1 para ver o exemplo ou digite seu palíndromo: ");
+
+        palindromo = enter.nextLine();
+
+        if (palindromo.equals("1")) {
+            System.out.println("-> Input: " + strExemplo);
+            encontraPalindromos(strExemplo);
+            System.out.println();
+
+        } else {
+            System.out.println("-> Input: " + palindromo);
+            encontraPalindromos(palindromo);
+            System.out.println();
+        }
+
+        enter.close();
+    }
+
+    public static void encontraPalindromos(String str){
+
+        Set<String> conjuntoPalindromo = new LinkedHashSet<>();
+
+        if(str.length() >= 3){
+            for(int i = 0; i < str.length();i++){
+                conjPalindromos(str, i, i, conjuntoPalindromo);
+                conjPalindromos(str, i, i + 1, conjuntoPalindromo);
+            }
+        } else {
+            conjuntoPalindromo.add("Por favor, digite uma palavra com 3 ou mais caracteres.");
+        }
+
+        Object[] palindromos = conjuntoPalindromo.toArray();
+
+        if(conjuntoPalindromo.isEmpty()){
+            System.out.println("Nenhum palíndromo foi encontrado.");
+        } else {
+            System.out.println("O(s) foi/foram:");
+            for(int i = 0; i < palindromos.length;i++){
+
+                String strP = (String) palindromos[i];
+
+                if(strP.length() >= 3){
+                    System.out.println(palindromos[i]);
+                }
+            }
+        }
+    }
+
+    private static void conjPalindromos(String str, int min, int max, Set<String> conjunto) {
+
+        while (min >= 0 && max < str.length() && str.charAt(min) == str.charAt(max)) {
+
+            conjunto.add(str.substring(min, max + 1));
+
+            min--;
+            max++;
+        }
+    }
+}
+
